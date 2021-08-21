38935b22899f5d0a955abe2db796eac1c48cf767
Skynire <74271247+Skynire@users.noreply.github.com>
2021-08-21 21:02:14 +0300

    Delete Project.py

--- Project.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome('C:/Users/Family/Desktop/Лев/My project/DriverChrome/chromedriver.exe')
-driver.get('https://www.youtube.com/')
-search = driver.find_element_by_xpath('//*[@id="search"]')
-search.send_keys('Hello, world!')
-#//*[@id="search-icon-legacy"]/yt-icon
-button
